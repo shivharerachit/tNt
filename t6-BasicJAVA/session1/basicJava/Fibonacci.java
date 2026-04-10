f4205ff35ee5219751213bf07ca07c24ed557053
Rachit Shivhare <rachitshivhare@gmail.com>
2026-04-10 16:43:21 +0530

    Basic Java Commit - 4 Added Fibonacci code

---
+++ t6-BasicJAVA/session1/basicJava/Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the upper limit: ");
+		int limit = sc.nextInt();
+
+		int first = 0;
+		int second = 1;
+
+		System.out.print("Fibonacci sequence up to " + limit + ": ");
+
+		while (first <= limit) {
+			System.out.print(first + " ");
+
+			int next = first + second;
+			first = second;
+			second = next;
+		}
+
+		System.out.println();
+		sc.close();
+	}
+}
